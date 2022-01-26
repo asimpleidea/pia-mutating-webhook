2d5428e8b533a748483d0697d1a46ca00db2053f
Elis Lulja <elis.lulja@gmail.com>
2022-01-26 21:33:11 +0100

    Validate sidecar image
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

--- main.go
+++ main.go
@@ -11,17 +11,29 @@ type AppOptions struct {
 	SidecarImage string
 }
 
+const (
+	CodeNoError int = iota
+	CodeNoSidecarImage
+)
+
 func main() {
 	opts := &AppOptions{}
 
 	flag.StringVar(&opts.SidecarImage, "sidecar-image", "",
 		"Image to inject as a sidecar")
 	flag.Parse()
+
+	os.Exit(run(opts))
 }
 
 func run(opts *AppOptions) int {
 	log := zerolog.New(os.Stderr)
 	log.Info().Msg("starting...")
 
-	return 0
+	if opts.SidecarImage == "" {
+		log.Error().Msg("no sidecar image provided")
+		return CodeNoSidecarImage
+	}
+
+	return CodeNoError
 }
