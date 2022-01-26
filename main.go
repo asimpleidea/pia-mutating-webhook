8ac2997b583cc8e77b414d961e9c601212bf2e20
Elis Lulja <elis.lulja@gmail.com>
2022-01-26 21:47:08 +0100

    Define server and readiness probe
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"flag"
 	"os"
+	"os/signal"
+	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
 
 type AppOptions struct {
 	SidecarImage string
+	DebugMode    bool
 }
+
+const (
+	fiberAppName string = "PIA Mutating Webhook"
+)
 
 const (
 	CodeNoError int = iota
@@ -21,19 +29,63 @@ func main() {
 
 	flag.StringVar(&opts.SidecarImage, "sidecar-image", "",
 		"Image to inject as a sidecar")
+	flag.BoolVar(&opts.DebugMode, "debug", false,
+		"Whether to show debug log lines")
 	flag.Parse()
 
 	os.Exit(run(opts))
 }
 
 func run(opts *AppOptions) int {
-	log := zerolog.New(os.Stderr)
+	log := zerolog.New(os.Stderr).Level(zerolog.InfoLevel)
 	log.Info().Msg("starting...")
+
+	// -----------------------------
+	// Parse options
+	// -----------------------------
 
 	if opts.SidecarImage == "" {
 		log.Error().Msg("no sidecar image provided")
 		return CodeNoSidecarImage
 	}
+
+	if opts.DebugMode {
+		log = log.Level(zerolog.DebugLevel)
+	}
+
+	// -----------------------------
+	// Server and paths
+	// -----------------------------
+
+	app := fiber.New(fiber.Config{
+		AppName:               fiberAppName,
+		ReadTimeout:           time.Minute,
+		DisableStartupMessage: opts.DebugMode,
+	})
+
+	app.Get("/readyz", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			log.Err(err).Msg("error while starting server")
+		}
+	}()
+
+	// -----------------------------
+	// Graceful shutdown & clean ups
+	// -----------------------------
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+
+	log.Info().Msg("shutting down...")
+	if err := app.Shutdown(); err != nil {
+		log.Err(err).Msg("error while waiting for server to shutdown")
+	}
+	log.Info().Msg("goodbye!")
 
 	return CodeNoError
 }
