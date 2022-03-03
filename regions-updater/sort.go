f9acd60ddaff7f729c6c2d59d7f1250ab32ed450
Elis Lulja <elis.lulja@gmail.com>
2022-03-03 20:52:45 +0100

    ✨ regions-updater: Create greater latency sort
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

--- regions-updater/sort.go
+++ regions-updater/sort.go
@@ -19,3 +19,23 @@ func (ll byLowerLatency) Less(i, j int) bool {
 func (ll byLowerLatency) Swap(i, j int) {
 	ll[i], ll[j] = ll[j], ll[i]
 }
+
+type byGreaterLatency []*ServerLatency
+
+func (ll byGreaterLatency) Len() int {
+	return len(ll)
+}
+
+func (ll byGreaterLatency) Less(i, j int) bool {
+	iserv, jserv := ll[i], ll[j]
+	if iserv.Latency == nil || jserv.Latency == nil {
+		// Don't sort
+		return false
+	}
+
+	return *iserv.Latency > *jserv.Latency
+}
+
+func (ll byGreaterLatency) Swap(i, j int) {
+	ll[i], ll[j] = ll[j], ll[i]
+}
